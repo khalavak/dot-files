df42985e7acfe5b41bc319f660af50e4fa386302
Edwin Jarvis <ejarvis@localhost.localdomain>
2013-02-25 17:38:55 +0200

    Added create-dotlinks.sh and modified the README file.

---
+++ create-dotlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to create the links in the home directory to the .dotfiles
+
+# Update the latest dotfiles from github
+
+git pull
+
+# Create the links
+
+ln -si ~/.dotfiles/.vimrc ~/.vimrc
+ln -si ~/.dotfiles/.vim ~/.vim
+ln -si ~/.dotfiles/.bashrc ~/.bashrc
+ln -si ~/.dotfiles/.conkyrc ~/.conkyrc                                            
+ln -si ~/.dotfiles/.i3 ~/.i3
+ln -si ~/.dotfiles/.i3status.conf ~/.i3status.conf
