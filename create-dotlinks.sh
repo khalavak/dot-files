257c96efe7f0a32a3508f392bbb689543cdbad1c
Kim Halavakoski <kim@deductivelabs.com>
2020-09-09 10:54:42 +0300

    Changed directory and disabled some link creation due to MacOS use

--- create-dotlinks.sh
+++ create-dotlinks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 # Script to create the links in the home directory to the .dotfiles
 
 # Update the latest dotfiles and git submodules from github
@@ -8,9 +8,9 @@ git submodule update
 
 # Create the links
 
-ln -si ~/.dotfiles/.vimrc ~/.vimrc
-ln -si ~/.dotfiles/.vim ~/.vim
-ln -si ~/.dotfiles/.bashrc ~/.bashrc
-#ln -si ~/.dotfiles/.i3 ~/.i3
-#ln -si ~/.dotfiles/.i3status.conf ~/.i3status.conf
-#ln -si ~/.dotfiles/.Xresources ~/.Xresources
+ln -si ~/.dot-files/.vimrc ~/.vimrc
+ln -si ~/.dot-files/.vim ~/.vim
+#ln -si ~/.dot-files/.bashrc ~/.bashrc
+#ln -si ~/.dot-files/.i3 ~/.i3
+#ln -si ~/.dot-files/.i3status.conf ~/.i3status.conf
+#ln -si ~/.dot-files/.Xresources ~/.Xresources
